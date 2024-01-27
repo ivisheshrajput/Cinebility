1f89e2fbcfa88af2e23ba291a4224c68093fd58c
ivisheshrajput <ivisheshrajput@gmail.com>
2024-01-27 17:24:34 +0530

    some new push

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Browse = () => {
-  return (
-    <div>Browse</div>
-  )
-}
+  return <div>Browsgbghdhbdrgnsnrte</div>;
+};
 
-export default Browse
+export default Browse;
