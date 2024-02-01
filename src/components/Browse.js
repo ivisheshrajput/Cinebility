ab4eee1a9a4844f6de12c4492daea6963677a648
Vishesh Rajput <156611608+ivisheshrajput@users.noreply.github.com>
2024-02-01 19:06:26 +0530

    Add files via upload

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import usePopularMovies from "../hooks/usePopularMovies";
 
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
   return (
     <div>
       <Header />
--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -7,7 +7,11 @@ const useNowPlayingMovies = () => {
   const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?age=1",
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+
+      // https://api.themoviedb.org/3/movie/popular
+      // https://api.themoviedb.org/3/movie/top_rated
+      // https://api.themoviedb.org/3/movie/upcoming
       API_OPTION
     );
     const nowPlayingList = await data.json();
